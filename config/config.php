c91394ebae810180313e942db1ffef63e85cbb5a
wangqiang <wangqiang@e.hunantv.com>
2016-03-01 10:54:09 +0800

    update

---
+++ config/config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 配置文件
+ *
+ */
+return array(
+    'app' => array(
+        'blog.fly.zxwqxtu.cc' => 'blog',
+        'crm.fly.zxwqxtu.cc' => 'crm',
+        'search.fly.zxwqxtu.cc' => 'search',
+    ),
+    'timezone' => 'Asia/Shanghai',
+    'debug' => true,
+    'errorLog' => '',
+    'charset' => 'UTF-8',
+    'indexController' => 'Test',
+    'importClassPath' => array(),
+    'require' => array(
+        'config/config.php'
+    ),
+    'cssHost' => 'http://css.static.zxwqxtu.cc',
+    'imageHost' => 'http://image.static.zxwqxtu.cc',
+    'jsHost' => 'http://js.static.zxwqxtu.cc',
+);
